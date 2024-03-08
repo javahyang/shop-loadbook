1c6c2790ed19f37f003eb146096fb2df0195b32c
이지향 <jihyang.lee@backpac.kr>
2024-03-08 16:09:46 +0900

    project init

---
+++ src/main/java/com/fastcampus/shoploadbook/ShopLoadbookApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.shoploadbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLoadbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopLoadbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/shoploadbook/ShopLoadbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.shoploadbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLoadbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
