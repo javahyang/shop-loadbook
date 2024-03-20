f4d3c2be75bf894163d8c417cb39ff894ea26618
javahyang <javahyang@gmail.com>
2024-03-20 14:04:47 +0900

    test: JavaFoo 테스트 커버리지 100%

--- src/test/java/com/fastcampus/shoploadbook/JavaFooTest.java
+++ src/test/java/com/fastcampus/shoploadbook/JavaFooTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.shoploadbook;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,5 +13,31 @@ public class JavaFooTest {
     public void partiallyCoveredHelloMethodTest() {
         String actual = javaFoo.hello("펭");
         assertEquals(actual, "하");
+    }
+
+    @Test
+    @DisplayName("callMe 호출")
+    void callMeTest() {
+        javaFoo.callMe();
+    }
+
+    @Test
+    @DisplayName("hello-hello 호출결과")
+    void helloHello() {
+        // when
+        String actual = javaFoo.hello("hello");
+
+        // then
+        assertEquals(actual, "world");
+    }
+
+    @Test
+    @DisplayName("hello-default 호출결과")
+    void helloDefault() {
+        // when
+        String actual = javaFoo.hello("귄");
+
+        // then
+        assertEquals(actual, "no one");
     }
 }
